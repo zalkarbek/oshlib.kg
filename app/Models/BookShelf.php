3c73b24dbd111d9bb1e41117f8cd7e2953ee2a1e
zalkarbek <kg.zalkar@gmail.com>
2022-05-05 10:42:05 +0600

    update bookshelf v2

--- app/Models/BookShelf.php
+++ app/Models/BookShelf.php
@@ -22,11 +22,8 @@ class BookShelf extends Model implements HasMedia
      */
     protected $fillable = [
         'name',
-<<<<<<< Updated upstream
         'user_id',
-=======
         'description',
->>>>>>> Stashed changes
         'is_public'
     ];
 
