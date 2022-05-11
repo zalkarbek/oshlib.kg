d183e895d127cb28369e4994e8ee0fc12e6acb43
TaalayDev <a.u.taalay@gmail.com>
2022-05-11 19:04:45 +0600

    removed summernote editors

--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -73,8 +73,6 @@
 <script src="{{asset('plugins/iCheck/icheck.min.js')}}"></script>
 <!-- select2 -->
 <script src="{{asset('plugins/select2/select2.min.js')}}"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{asset('plugins/summernote/summernote-bs4.min.js')}}"></script>
 {{--dropzone--}}
 <script src="{{asset('plugins/dropzone/dropzone.js')}}"></script>
 <script type="text/javascript">
