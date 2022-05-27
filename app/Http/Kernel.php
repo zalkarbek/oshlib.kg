7b4b11a2273ec4af0e8ca6736130ade1ee9d3a9e
kg.zalkar <kg.zalkar@gmail.com>
2022-05-27 18:54:21 +0600

    fix server

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,11 +16,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-<<<<<<< HEAD
-        // \Fruitcake\Cors\HandleCors::class,
-=======
         //\Fruitcake\Cors\HandleCors::class,
->>>>>>> ecae5def43174fca86fadf20825a69dffdc8e8f2
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
